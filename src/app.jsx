6c712ffbf6d6a1fefcc8e70b5fd2f45b5a73fc7d
Joseph Miser <josephmiser5@gmail.com>
2026-02-08 14:18:11 -0700

    updated app.jsx with html components needed to creat header and footer elements for the site

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,47 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 
 export default function App() {
-  return <div className="app bg-dark text-light">App will display here</div>;
+  return (
+    <div className="app bg-dark text-light">
+      <header className="d-flex justify-content-between align-items-center">
+        <h1 className="mb-0 mt-0">Tic-Tac-Toe</h1>
+
+        <nav>
+          <menu className="d-flex gap-3 mb-0">
+            <li>
+              <a className="nav-link text-white p-0" href="index.html">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="nav-link text-white p-0" href="play.html">
+                Play
+              </a>
+            </li>
+            <li>
+              <a className="nav-link text-white p-0" href="friends.html">
+                Friends
+              </a>
+            </li>
+            <li>
+              <a className="nav-link text-white p-0" href="win-loss.html">
+                Win-Loss
+              </a>
+            </li>
+          </menu>
+        </nav>
+      </header>
+      <main>App component will be displayed here</main>
+      <footer className="text-center">
+        <hr />
+        <span className="text-reset">Joseph Miser</span>
+        <a
+          className="text-white ms-3"
+          href="https://github.com/josephmiser5/tic-tac-toe"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
